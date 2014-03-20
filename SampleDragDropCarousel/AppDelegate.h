f62f203bae4cd25c8caca9330ddf8bac32ebdb43
Firdous <firdous@mohsin.local>
2014-03-20 12:03:52 +0500

    Initial Commit

---
+++ SampleDragDropCarousel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleDragDropCarousel
+//
+//  Created by Firdous on 20/03/2014.
+//  Copyright (c) 2014 Firdous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleDragDropCarousel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleDragDropCarousel
+//
+//  Created by Firdous on 20/03/2014.
+//  Copyright (c) 2014 Firdous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
